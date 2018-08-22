2363fdb92781e1985e9ec141d38d7c7aa31626dc
Konstantin Savransky <ksavransky@gmail.com>
2018-08-22 14:07:46 -0700

    localStoreworking

--- components/Categories.js
+++ components/Categories.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { Avatar, List, ListItem } from 'react-native-elements'
+import { AsyncStorage } from 'react-native';
+import { List, ListItem } from 'react-native-elements'
 import { sortBy } from 'lodash'
 
 
 // props
-// difficulty - string - from Category screen
-// categoryName - string - from data file
 // totalPuzzles - int - from data file
 // completedPuzzles - int - from local memory NOTE: probably want to store array with key names in local memory for complete, then use length here
-// categoryImageUrl - string
 // points - int - from local memory
 
 export default class Categories extends Component {
+
+  componentWillMount() {
+    this._storeData()
+  }
+
+  _storeData = async () => {
+    try {
+      await AsyncStorage.setItem('@AHH:hi:no', '@AHH:hi:no!!');
+    } catch (error) {
+      console.warn('error in _storeData', error);
+    }
+  }
+
+
+  _retrieveData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('@AHH:hi:no');
+        if (value !== null) {
+          console.warn('in _retrieveData value:');
+          console.warn(value);
+        }
+      } catch (error) {
+        console.warn('error in _retrieveData', error);
+      }
+  }
+
+
   render() {
     sortBy(this.props.categories, ['name']).forEach((category) => {
-      console.warn(category.name)
-      console.warn(Object.keys(category.levels).length)
+      // console.warn(category.name)
+      // console.warn(Object.keys(category.levels).length)
     })
     return (
       <List containerStyle={{width: '90%'}}>
@@ -26,6 +50,7 @@ export default class Categories extends Component {
               avatar={category.iconURL}
               key={category.name}
               title={category.name}
+              onPress={this._retrieveData}
             />
           ))
         }
--- screens/ChooseDifficulty.js
+++ screens/ChooseDifficulty.js
@@ -11,17 +11,17 @@ export default class ChooseDifficulty extends Component {
       <View style={[containerStyle.centeredBoth, backgroundColorStyle.lightBlue]}>
         <Text h3 fontFamily='ChalkboardSE' style={{color: 'blue', marginBottom: 100}}>Choose Difficulty</Text>
         <LargeButton
-          onPress={() => this.props.navigation.navigate('Categories', {difficulty: 'Easy'})}
+          onPress={() => this.props.navigation.navigate('CategoriesScreen', {difficulty: 'Easy'})}
           backgroundColor='#28a745'
           style={{marginBottom: 50}}
           title='EASY' />
         <LargeButton
-          onPress={() => this.props.navigation.navigate('Categories', {difficulty: 'Medium'})}
+          onPress={() => this.props.navigation.navigate('CategoriesScreen', {difficulty: 'Medium'})}
           backgroundColor='orange'
           style={{marginBottom: 50}}
           title='MEDIUM' />
         <LargeButton
-          onPress={() => this.props.navigation.navigate('Categories', {difficulty: 'Hard'})}
+          onPress={() => this.props.navigation.navigate('CategoriesScreen', {difficulty: 'Hard'})}
           backgroundColor='red'
           style={{marginBottom: 50}}
           title='HARD' />
