eb9f08fc528f249ab50a11337b2d28e7e77fdb6c
Konstantin Savransky <ksavransky@gmail.com>
2018-08-23 15:14:01 -0700

    more pics

--- data/Data.js
+++ data/Data.js
@@ -4,22 +4,16 @@ export const Data = {
       name: 'Places',
       iconURL: require('../assets/images/places/eiffel.jpeg'),
       levels: [
-        {'colosseum': 'https://commons.wikimedia.org/wiki/Colosseum#/media/File:ColosseumAtEvening.jpg'},
-        {'notre dame': 'https://commons.wikimedia.org/wiki/Cath%C3%A9drale_Notre-Dame_de_Paris'},
-        {'great wall china': 'https://upload.wikimedia.org/wikipedia/commons/5/50/Badaling_China_Great-Wall-of-China-01.jpg'},
-        {'sydney opera house': 'https://commons.wikimedia.org/wiki/Sydney_Opera_House#/media/File:Sydney_Opera_House_-_Dec_2008.jpg'}
-      ]
-    },
-    {
-      name: 'Landmarks',
-      iconURL: require('../assets/images/places/eiffel.jpeg'),
-      levels: [
+        {'colosseum': require('../assets/images/places/colosseum.jpeg')},
+        {'notre dame': require('../assets/images/places/notredame.jpeg')},
+        {'great wall china': require('../assets/images/places/greatwall.jpg')},
+        {'sydney opera house': require('../assets/images/places/sydneyoperahouse.jpg')},
         {'eiffel tower': require('../assets/images/places/eiffel.jpeg')},
-        {'big ben': 'https://commons.wikimedia.org/wiki/Big_Ben#/media/File:Clock_Tower_-_Palace_of_Westminster,_London_-_September_2006.jpg'},
-        {'statue liberty': 'https://commons.wikimedia.org/wiki/Statue_of_Liberty#/media/File:Statue-of-liberty_tysto.jpg'},
-        {'golden gate': 'https://commons.wikimedia.org/wiki/Golden_Gate_Bridge#/media/File:GG-ftpoint-bridge-2.jpg'},
-        {'pyramid giza': 'https://commons.wikimedia.org/wiki/Great_Pyramid_of_Giza#/media/File:Kheops-Pyramid.jpg'},
-        {'taj mahal': 'https://commons.wikimedia.org/wiki/Taj_Mahal#/media/File:Taj_Mahal_in_March_2004.jpg'}
+        {'big ben': require('../assets/images/places/bigben.jpeg')},
+        {'statue liberty': require('../assets/images/places/statueofliberty.jpeg')},
+        {'golden gate': require('../assets/images/places/goldengate.jpeg')},
+        {'pyramid giza': require('../assets/images/places/pyramidofgiza.jpg')},
+        {'taj mahal': require('../assets/images/places/tajmahal.jpg')}
       ]
     }
   ],
