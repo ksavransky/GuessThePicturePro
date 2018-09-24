014e42d60de95aeae247bdb96d3b5e1cc1f6e51f
Konstantin Savransky <ksavransky@gmail.com>
2018-09-24 09:10:41 -0700

    more

--- data/easy/Places.js
+++ data/easy/Places.js
@@ -1,49 +1,49 @@
 export const EasyPlaces = [
+  { answer: 'big_ben',
+    imagePath: require('../../assets/images/levels/places/easy/bigben.jpeg'),
+    isCompleted: false,
+    hint: 'Keeping time for London since 1859!'
+  },
   { answer: 'colosseum',
     imagePath: require('../../assets/images/levels/places/easy/colosseum.jpeg'),
     isCompleted: false,
     hint: 'When in Rome... check out the gladiators!'
-  },
-  { answer: 'notre_dame',
-    imagePath: require('../../assets/images/levels/places/easy/notredame.jpeg'),
-    isCompleted: false,
-    hint: 'A french dame. Kinda.'
-  },
-  { answer: 'great_wall_china',
-    imagePath: require('../../assets/images/levels/places/easy/greatwall.jpg'),
-    isCompleted: false,
-    hint: 'This wall can be seen from space!'
-  },
-  { answer: 'sydney_opera_house',
-    imagePath: require('../../assets/images/levels/places/easy/sydneyoperahouse.jpg'),
-    isCompleted: false,
-    hint: 'See a little culture down under.'
   },
   { answer: 'eiffel_tower',
     imagePath: require('../../assets/images/levels/places/easy/eiffel.jpeg'),
     isCompleted: false,
     hint: 'You know this! Check your spelling.'
   },
-  { answer: 'big_ben',
-    imagePath: require('../../assets/images/levels/places/easy/bigben.jpeg'),
+  { answer: 'great_wall_china',
+    imagePath: require('../../assets/images/levels/places/easy/greatwall.jpg'),
     isCompleted: false,
-    hint: 'Keeping time for London since 1859!'
-  },
-  { answer: 'statue_liberty',
-    imagePath: require('../../assets/images/levels/places/easy/statueofliberty.jpeg'),
-    isCompleted: false,
-    hint: 'You know this! Check your spelling.'
+    hint: 'This wall can be seen from space!'
   },
   { answer: 'golden_gate',
     imagePath: require('../../assets/images/levels/places/easy/goldengate.jpg'),
     isCompleted: false,
     hint: 'This bridge looks amazing even in the SF fog!'
   },
+  { answer: 'notre_dame',
+    imagePath: require('../../assets/images/levels/places/easy/notredame.jpeg'),
+    isCompleted: false,
+    hint: 'A french dame. Kinda.'
+  },
   { answer: 'pyramid_giza',
     imagePath: require('../../assets/images/levels/places/easy/pyramidofgiza.jpg'),
     isCompleted: false,
     optionalAnswer: [['pyramid', 'giza'], ['great', 'pyramid']],
     hint: 'The most famous ancient Egyptian structure.'
+  },
+  { answer: 'statue_liberty',
+    imagePath: require('../../assets/images/levels/places/easy/statueofliberty.jpeg'),
+    isCompleted: false,
+    hint: 'You know this! Check your spelling.'
+  },
+  { answer: 'sydney_opera_house',
+    imagePath: require('../../assets/images/levels/places/easy/sydneyoperahouse.jpg'),
+    isCompleted: false,
+    hint: 'See a little culture down under.'
   },
   { answer: 'taj_mahal',
     imagePath: require('../../assets/images/levels/places/easy/tajmahal.jpg'),
