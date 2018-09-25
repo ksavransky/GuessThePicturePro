319e231f480f730789b54640b94f039bee676ba4
Konstantin Savransky <ksavransky@gmail.com>
2018-09-25 08:48:39 -0700

    more data

--- data/easy/Landmarks.js
+++ data/easy/Landmarks.js
@@ -1,8 +1,19 @@
 export const EasyLandmarks = [
+  { answer: 'bean',
+    imagePath: require('../../assets/images/levels/landmarks/easy/bean.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['bean'], ['cloud', 'gate']],
+    hint: 'In Chicago. Looks like a ______!'
+  },
   { answer: 'big_ben',
     imagePath: require('../../assets/images/levels/landmarks/easy/bigben.jpeg'),
     isCompleted: false,
     hint: 'Keeping time for London since 1859!'
+  },
+  { answer: 'central_park',
+    imagePath: require('../../assets/images/levels/landmarks/easy/central-park.jpg'),
+    isCompleted: false,
+    hint: "New York's famous huge park."
   },
   { answer: 'colosseum',
     imagePath: require('../../assets/images/levels/landmarks/easy/colosseum.jpeg'),
@@ -14,15 +25,41 @@ export const EasyLandmarks = [
     isCompleted: false,
     hint: 'You know this! Check your spelling.'
   },
-  { answer: 'great_wall_china',
-    imagePath: require('../../assets/images/levels/landmarks/easy/greatwall.jpg'),
+  { answer: 'empire_state',
+    imagePath: require('../../assets/images/levels/landmarks/easy/empire-state-building.jpg'),
     isCompleted: false,
-    hint: 'This wall can be seen from space!'
+    hint: 'A famous old New York skyscraper.'
   },
   { answer: 'golden_gate',
     imagePath: require('../../assets/images/levels/landmarks/easy/goldengate.jpg'),
     isCompleted: false,
     hint: 'This bridge looks amazing even in the SF fog!'
+  },
+  { answer: 'grand_central',
+    imagePath: require('../../assets/images/levels/landmarks/easy/grand-central-station.jpg'),
+    isCompleted: false,
+    hint: 'Main subway stop in New York!'
+  },
+  { answer: 'grand_canyon',
+    imagePath: require('../../assets/images/levels/landmarks/easy/grandcanyon.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['grand', 'canyon'], ['horseshoe', 'bend']],
+    hint: 'Ahhh, a big hole in the ground in the USA Southwest.'
+  },
+  { answer: 'great_wall_china',
+    imagePath: require('../../assets/images/levels/landmarks/easy/greatwall.jpg'),
+    isCompleted: false,
+    hint: 'This wall can be seen from space!'
+  },
+  { answer: 'leaning_tower_pisa',
+    imagePath: require('../../assets/images/levels/landmarks/easy/leaning-tower-pisa.jpg'),
+    isCompleted: false,
+    hint: 'Those Italian engineers need some help!'
+  },
+  { answer: 'london_eye',
+    imagePath: require('../../assets/images/levels/landmarks/easy/londoneye.jpeg'),
+    isCompleted: false,
+    hint: "'I' bed you can figure out this London landmark!"
   },
   { answer: 'notre_dame',
     imagePath: require('../../assets/images/levels/landmarks/easy/notredame.jpeg'),
