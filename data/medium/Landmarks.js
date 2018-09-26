44049463ede11ad42331de1cef1c012dfa1b38a9
Konstantin Savransky <ksavransky@gmail.com>
2018-09-25 21:46:20 -0700

    medium landmark progress

--- data/medium/Landmarks.js
+++ data/medium/Landmarks.js
@@ -4,5 +4,36 @@ export const MediumLandmarks = [
     isCompleted: false,
     optionalAnswer: [['acropolis'], ['parthenon']],
     hint: 'Athens. Famous ancient ruin site.'
-  }
+  },
+  { answer: 'alhambra',
+    imagePath: require('../../assets/images/levels/landmarks/medium/alhambra.jpg'),
+    isCompleted: false,
+    hint: 'A beautiful Moorish palace in Granada.'
+  },
+  { answer: 'angkor_wat',
+    imagePath: require('../../assets/images/levels/landmarks/medium/angkor-wat.jpg'),
+    isCompleted: false,
+    hint: "In Cambodia. The largest religious monument in the world. 'What' can it be?"
+  },
+  { answer: 'brooklyn_bridge',
+    imagePath: require('../../assets/images/levels/landmarks/medium/brooklynbridge.jpg'),
+    isCompleted: false,
+    hint: "This New York bridge connects Manhattan with the borough it's named after."
+  },
+  { answer: 'buckingham_palace',
+    imagePath: require('../../assets/images/levels/landmarks/medium/buckingham-palace.jpg'),
+    isCompleted: false,
+    hint: "The Queen lives here!"
+  },
+  { answer: 'chichen_itza',
+    imagePath: require('../../assets/images/levels/landmarks/medium/chichen-itza.jpg'),
+    isCompleted: false,
+    hint: "Mayan pyramid in Mexico. Two words. First word is one letter away from chicken!"
+  },
+  { answer: 'cristo_redentor',
+    imagePath: require('../../assets/images/levels/landmarks/medium/cristo-redentor-christ-redeemer.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['cristo', 'redentor'], ['redeemer', 'christ']],
+    hint: "Famous Rio de Janeiro statue of Christ"
+  },
 ]
