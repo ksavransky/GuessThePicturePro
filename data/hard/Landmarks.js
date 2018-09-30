3dab4fe9dc4bc3e41c822fbf67ff2207ac8d5900
Konstantin Savransky <ksavransky@gmail.com>
2018-09-30 08:00:02 -0700

    hard landmarsk done

--- data/hard/Landmarks.js
+++ data/hard/Landmarks.js
@@ -2,6 +2,87 @@ export const HardLandmarks = [
   { answer: 'reichstag.jpg',
     imagePath: require('../../assets/images/levels/landmarks/hard/berlin-reichstag.jpg'),
     isCompleted: false,
-    hint: 'Berlin capitol building.'
+    hint: 'A building in Berlin that is the meeting place of the Bundestag (parliament).'
+  },
+  { answer: 'burj.jpg',
+    imagePath: require('../../assets/images/levels/landmarks/hard/Burj-Khalifa-or-tower.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['burj', 'khalifa'], ['burj', 'dubai']],
+    hint: 'In Dubai. Tallest building in the world! 163 floors!'
+  },
+  { answer: 'castelstangelo.jpg',
+    imagePath: require('../../assets/images/levels/landmarks/hard/castelstangelo.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['castelstangelo'], ['castel', "sant'angelo"], ['castel', "angelo"], ['castle', "angelo"], ['mausoleum', 'hadrian']],
+    hint: 'In Rome. It was initially commissioned by Emperor Hadrian. Later used by the popes as a fortress and castle.'
+  },
+  { answer: 'moher.jpg',
+    imagePath: require('../../assets/images/levels/landmarks/hard/cliffs-of-moher.jpg'),
+    isCompleted: false,
+    hint: 'Sea cliffs located at the southwestern edge of the Burren region in County Clare, Ireland.'
+  },
+  { answer: 'gardens_bay.jpg',
+    imagePath: require('../../assets/images/levels/landmarks/hard/Gardens-by-the-Bay.jpg'),
+    isCompleted: false,
+    hint: 'A nature park spanning 101 hectares of reclaimed land in the Central Region of Singapore.'
+  },
+  { answer: 'grand_canal',
+    imagePath: require('../../assets/images/levels/landmarks/hard/grand-canal.jpg'),
+    isCompleted: false,
+    hint: 'Famous channel in Venice.'
+  },
+  { answer: 'grand',
+    imagePath: require('../../assets/images/levels/landmarks/hard/grand-prismatic-spring-yellowstone.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['yellowstone'], ['prismatic', 'spring'], ['grand', 'spring']],
+    hint: 'Largest hot spring in the United States.'
+  },
+  { answer: 'hagia_sophia',
+    imagePath: require('../../assets/images/levels/landmarks/hard/hagia-sophia.jpg'),
+    isCompleted: false,
+    hint: 'Former Greek Orthodox Christian patriarchal cathedral, later an Ottoman imperial mosque and now a museum in Istanbul.'
+  },
+  { answer: "iguazu",
+    imagePath: require("../../assets/images/levels/landmarks/hard/iguazu-falls.jpg"),
+    isCompleted: false,
+    optionalAnswer: [['Iguaçu'], ['iguazu']],
+    hint: "In Argentina. The largest waterfall system in the world."
+  },
+  { answer: "matterhorn",
+    imagePath: require("../../assets/images/levels/landmarks/hard/matterhorn.jpg"),
+    isCompleted: false,
+    hint: "A mountain int the Alps. Straddling the border between Switzerland and Italy."
+  },
+  { answer: "orsay",
+    imagePath: require("../../assets/images/levels/landmarks/hard/Musee-d'Orsay.jpg"),
+    isCompleted: false,
+    optionalAnswer: [["musee", "orsay"], ["musee", "d'orsay"], ["musee", "d' orsay"], ["d'orsay"]],
+    hint: "A museum in Paris. Housed in a former railway station. It holds mainly French art dating from 1848 to 1914."
+  },
+  { answer: "guell",
+    imagePath: require("../../assets/images/levels/landmarks/hard/park-guell.jpg"),
+    isCompleted: false,
+    hint: "A park in Barcelona desgined by Antoni Gaudi."
+  },
+  { answer: "schonbrunn",
+    imagePath: require("../../assets/images/levels/landmarks/hard/Schonbrunn-Palace.jpg"),
+    isCompleted: false,
+    hint: "A 1,441-room Baroque palace and former imperial summer residence located in Vienna."
+  },
+  { answer: "spanish_steps",
+    imagePath: require("../../assets/images/levels/landmarks/hard/spanish-steps.jpg"),
+    isCompleted: false,
+    hint: "A set of steps in Rome, climbing between the Piazza di Spagna and Piazza Trinita dei Monti."
+  },
+  { answer: "table",
+    imagePath: require("../../assets/images/levels/landmarks/hard/table-mountain.jpg"),
+    isCompleted: false,
+    optionalAnswer: [["cape", "town"], ["table", "mountain"]],
+    hint: "A famous South African mountain with a flat-topped. Or the city it overlooks."
+  },
+  { answer: "victoria_falls",
+    imagePath: require("../../assets/images/levels/landmarks/hard/victoria-falls.jpg"),
+    isCompleted: false,
+    hint: "The world's largest waterfall. In southern Africa between Zambia and Zimbabwe. Named after a British Monarch."
   }
 ]
