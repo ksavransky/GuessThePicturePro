7822dc715f0bf6a361380dc2a9943226fe8e3955
Konstantin Savransky <ksavransky@gmail.com>
2018-10-01 09:27:48 -0700

    started easy art

---
+++ data/easy/Art.js
@@ -0,0 +1,43 @@
+export const EasyArt = [
+  { answer: 'david',
+    imagePath: require('../../assets/images/levels/art/easy/david.jpg'),
+    isCompleted: false,
+    hint: "Michaelangelo's most famous sculpture of a biblical hero."
+  },
+  { answer: 'mona_lisa',
+    imagePath: require('../../assets/images/levels/art/easy/mona-lisa.jpg'),
+    isCompleted: false,
+    hint: "It might be Da Vince's self-portrait as a woman."
+  },
+  { answer: 'water_lilies',
+    imagePath: require('../../assets/images/levels/art/easy/water_lilies.jpg'),
+    isCompleted: false,
+    hint: "Monet's famous flowers in a pond."
+  },
+  { answer: 'sistine_chapel',
+    imagePath: require('../../assets/images/levels/art/easy/sistine-chapel.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['creation', 'adam'], ['sistine', 'chapel']],
+    hint: "Michaelangelo's masterpiece is one of many fresco's on this Vatican ceiling."
+  },
+  { answer: 'starry_night',
+    imagePath: require('../../assets/images/levels/art/easy/starry-night.jpg'),
+    isCompleted: false,
+    hint: "This dreamy Van Gogh masterpiece's title is pretty much what it looks like."
+  },
+  { answer: 'girl_pearl_earring',
+    imagePath: require('../../assets/images/levels/art/easy/The_Girl_With_The_Pearl_Earring.jpg'),
+    isCompleted: false,
+    hint: "Vermeer's painting's title is straightforward. It depicts a _____ with a ______ _______."
+  },
+  { answer: 'last_supper',
+    imagePath: require('../../assets/images/levels/art/easy/the-last-supper.jpg'),
+    isCompleted: false,
+    hint: "You know this. It's Jesus having his final meal with his apostles."
+  },
+  { answer: 'thinker',
+    imagePath: require('../../assets/images/levels/art/easy/the-thinker.jpg'),
+    isCompleted: false,
+    hint: "Rodin's famous sculpture of a man contemplating stuff."
+  },
+]
