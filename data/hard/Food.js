beb2711e531edfad5f7d5b5b30e29c3ecde94db5
Konstantin Savransky <ksavransky@gmail.com>
2018-09-30 17:28:33 -0700

    more hard food

--- data/hard/Food.js
+++ data/hard/Food.js
@@ -26,6 +26,12 @@ export const HardFood = [
     isCompleted: false,
     hint: "A Japanese dumpling and sweet made from mochiko."
   },
+  { answer: 'macarons',
+    imagePath: require('../../assets/images/levels/food/hard/macarons.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['macarons'], ['macaron']],
+    hint: "Tasty french sweet meringue-based confection."
+  },
   { answer: 'pierogi',
     imagePath: require('../../assets/images/levels/food/hard/Pierogi-polish-dumpling.jpg'),
     isCompleted: false,
@@ -37,10 +43,20 @@ export const HardFood = [
     optionalAnswer: [['portuguese', 'custard', 'tart'], ['pastel', 'de', 'nata'], ['pastel', 'denata']],
     hint: "A Portuguese egg tart pastry dusted with cinnamon."
   },
+  { answer: 'ramen',
+    imagePath: require('../../assets/images/levels/food/hard/ramen.jpg'),
+    isCompleted: false,
+    hint: "Japanese wheat noodles served in a meat or fish-based broth."
+  },
   { answer: 'rendang',
     imagePath: require('../../assets/images/levels/food/hard/rendang.jpg'),
     isCompleted: false,
     hint: "A spicy meat dish which originated from Indonesia."
+  },
+  { answer: 'schnitzel',
+    imagePath: require('../../assets/images/levels/food/hard/schnitzel.jpeg'),
+    isCompleted: false,
+    hint: "Austrian dish. Meat tenderizer, fried in some kind of oil or fat."
   },
   { answer: 'shakshuka',
     imagePath: require('../../assets/images/levels/food/hard/SHAKSHUKA-kagyana.jpg'),
@@ -48,9 +64,20 @@ export const HardFood = [
     optionalAnswer: [['shakshuka'], ['kagyana']],
     hint: "A Mediterranean and Middle Eastern dish of eggs poached in a sauce of tomatoes, chili peppers, and onions."
   },
+  { answer: 'souffle',
+    imagePath: require('../../assets/images/levels/food/hard/souffle.jpg'),
+    isCompleted: false,
+    hint: "A baked egg-based dish which originated in early eighteenth century France."
+  },
   { answer: 'tarte_tatin',
     imagePath: require('../../assets/images/levels/food/hard/Tarte-tatin.jpg'),
     isCompleted: false,
-    hint: "Named after the hotel serving it as its signature dish in Lamotte-Beuvron, France"
+    hint: "Named after the hotel serving it as its signature dish in Lamotte-Beuvron, France."
+  },
+  { answer: 'upside-down-cake',
+    imagePath: require('../../assets/images/levels/food/hard/upside-down-cake.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['upside', 'down', 'cake'], ['upsidedown', 'cake']],
+    hint: "A cake that is baked in a single pan with its toppings at the bottom of the pan."
   }
 ]
