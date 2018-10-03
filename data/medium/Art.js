701b98758892295d9bf8c8c3eb80161a6afe8312
Konstantin Savransky <ksavransky@gmail.com>
2018-10-02 22:24:12 -0700

    adding hints to medium paintings

--- data/medium/Art.js
+++ data/medium/Art.js
@@ -2,100 +2,108 @@ export const MediumArt = [
   { answer: 'david',
     imagePath: require('../../assets/images/levels/art/medium/bernini-david.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "Gian Lorenzo Bernini's sculpture of a biblical hero."
   },
-  { answer: 'terrace_night',
+  { answer: 'cafe',
     imagePath: require('../../assets/images/levels/art/medium/Cafe_Terrace_at_Night.jpg'),
     isCompleted: false,
-    hint: ""
+    optionalAnswer: [['cafe', 'terrace', 'night'], ['cafe', 'terrace', 'forum'], ['coffeehouse', 'evening']],
+    hint: "Vincent van Gogh painted this late scene of a cafe in Arles, France, in mid-September 1888."
   },
   { answer: 'batilo',
     imagePath: require('../../assets/images/levels/art/medium/casa-batilo.jpg'),
     isCompleted: false,
-    hint: ""
+    optionalAnswer: [['casa', 'batilo'], ['batllo', 'casa']],
+    hint: "A building in the center of Barcelona. It was designed by Antoni Gaudi."
   },
   { answer: 'david',
     imagePath: require('../../assets/images/levels/art/medium/David-donatello.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "Donatello's sculpture of a biblical hero."
   },
   { answer: 'drac',
     imagePath: require('../../assets/images/levels/art/medium/el-drac.jpg'),
     isCompleted: false,
-    hint: ""
+    optionalAnswer: [['drac'], ['dragon']],
+    hint: "Gaudi's multicolored mosaic salamander in Park Guell. Although it's a salamander, it has a more fearsome name."
   },
   { answer: 'female',
     imagePath: require('../../assets/images/levels/art/medium/Female_head_(La_Scapigliata)_or_Woman.jpg'),
     isCompleted: false,
-    optionalAnswer: [['female', 'head'], ['scapigliata'], ['woman']],
-    hint: ""
+    optionalAnswer: [['female', 'head'], ['scapigliata'], ['woman', 'head']],
+    hint: " Leonardo da Vinci's painting, dating from perhaps around 1500. It's a ____ of a _____."
   },
   { answer: 'guernica',
     imagePath: require('../../assets/images/levels/art/medium/guernica.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "Picasso's famous work. Painted as a reaction to the Nazi's devastating bombing practice on a Basque town."
   },
   { answer: 'irisis',
     imagePath: require('../../assets/images/levels/art/medium/irisis.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "Vincent van Gogh's painting of these flowers, in the last year before his death in 1890."
   },
   { answer: 'lady_ermine',
     imagePath: require('../../assets/images/levels/art/medium/Lady_with_an_Ermine.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "A painting by Leonardo da Vinci from around 1489–1490 and one of Poland's national treasures. The portrait's subject is Cecilia Gallerani with this fury creature."
   },
   { answer: 'meninas',
     imagePath: require('../../assets/images/levels/art/medium/Las_Meninas.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "A 1656 painting by Diego Velázquez. It creates an uncertain relationship between the viewer and the figures depicted."
   },
-  { answer: '_____',
-    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+  { answer: 'moulin_galette',
+    imagePath: require('../../assets/images/levels/art/medium/Le_Moulin_de_la_Galette.jpg'),
     isCompleted: false,
-    optionalAnswer: [['', '']],
-    hint: ""
+    hint: "A 1876 painting by Renoir. One of Impressionism's most celebrated masterpieces. The painting depicts a Sunday afternoon at a venue in the district of Montmartre."
   },
-  { answer: '_____',
-    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+  { answer: 'creation',
+    imagePath: require('../../assets/images/levels/art/medium/The_Creation_of_Adam.jpg'),
     isCompleted: false,
-    optionalAnswer: [['', '']],
-    hint: ""
+    optionalAnswer: [['creation', 'adam'], ['sistine', 'chapel']],
+    hint: "A fresco painting by Michelangelo, which forms part of a famous vatican ceiling."
   },
-  { answer: '_____',
-    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+  { answer: 'wave',
+    imagePath: require('../../assets/images/levels/art/medium/The_Great_Wave_off_Kanagawa_or_Wave.jpg'),
     isCompleted: false,
-    optionalAnswer: [['', '']],
-    hint: ""
+    hint: "A woodblock print by the Japanese ukiyo-e artist Hokusai. It was published sometime between 1829 and 1833."
   },
-  { answer: '_____',
-    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+  { answer: 'kiss',
+    imagePath: require('../../assets/images/levels/art/medium/The_Kiss.jpg'),
     isCompleted: false,
-    optionalAnswer: [['', '']],
-    hint: ""
+    optionalAnswer: [['the', 'kiss'], ['the', 'lovers'], ['Liebespaar']],
+    hint: "An oil painting, with added silver and gold leaf by the Austrian Symbolist painter Gustav Klimt."
   },
-  { answer: '_____',
-    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+  { answer: 'night_watch',
+    imagePath: require('../../assets/images/levels/art/medium/The_Night_Watch.jpg'),
     isCompleted: false,
-    optionalAnswer: [['', '']],
-    hint: ""
+    hint: "A 1642 painting by Rembrandt van Rijn. It depicts a group doing its late rounds."
   },
-  { answer: '_____',
-    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+  { answer: 'water_lily_pond',
+    imagePath: require('../../assets/images/levels/art/medium/The_Water-Lily_Pond.jpg'),
     isCompleted: false,
-    optionalAnswer: [['', '']],
     hint: ""
   },
-  { answer: '_____',
-    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+  { answer: 'birth_venus',
+    imagePath: require('../../assets/images/levels/art/medium/the-birth-of-venus.jpg'),
     isCompleted: false,
-    optionalAnswer: [['', '']],
     hint: ""
   },
-  { answer: '_____',
-    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+  { answer: 'kiss',
+    imagePath: require('../../assets/images/levels/art/medium/the-kiss.jpg'),
     isCompleted: false,
-    optionalAnswer: [['', '']],
     hint: ""
   },
+  { answer: 'vangoghselfportrait',
+    imagePath: require('../../assets/images/levels/art/medium/vangoghselfportrait.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['van', 'gogh'], ['self', 'portrait']],
+    hint: "This Post-Impressionism drew himself many times. This being one of his most famous depictions."
+  },
+  { answer: 'wheatfield_crows',
+    imagePath: require('../../assets/images/levels/art/medium/Wheatfield_with_crows.jpg'),
+    isCompleted: false,
+    hint: ""
+  }
 ]
