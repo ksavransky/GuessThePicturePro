dc58098e107b3cbbff274a7c137be845231d9ca5
Konstantin Savransky <ksavransky@gmail.com>
2018-10-02 17:50:26 -0700

    medium art wip

---
+++ data/medium/Art.js
@@ -0,0 +1,101 @@
+export const MediumArt = [
+  { answer: 'david',
+    imagePath: require('../../assets/images/levels/art/medium/bernini-david.jpg'),
+    isCompleted: false,
+    hint: ""
+  },
+  { answer: 'terrace_night',
+    imagePath: require('../../assets/images/levels/art/medium/Cafe_Terrace_at_Night.jpg'),
+    isCompleted: false,
+    hint: ""
+  },
+  { answer: 'batilo',
+    imagePath: require('../../assets/images/levels/art/medium/casa-batilo.jpg'),
+    isCompleted: false,
+    hint: ""
+  },
+  { answer: 'david',
+    imagePath: require('../../assets/images/levels/art/medium/David-donatello.jpg'),
+    isCompleted: false,
+    hint: ""
+  },
+  { answer: 'drac',
+    imagePath: require('../../assets/images/levels/art/medium/el-drac.jpg'),
+    isCompleted: false,
+    hint: ""
+  },
+  { answer: 'female',
+    imagePath: require('../../assets/images/levels/art/medium/Female_head_(La_Scapigliata)_or_Woman.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['female', 'head'], ['scapigliata'], ['woman']],
+    hint: ""
+  },
+  { answer: 'guernica',
+    imagePath: require('../../assets/images/levels/art/medium/guernica.jpg'),
+    isCompleted: false,
+    hint: ""
+  },
+  { answer: 'irisis',
+    imagePath: require('../../assets/images/levels/art/medium/irisis.jpg'),
+    isCompleted: false,
+    hint: ""
+  },
+  { answer: 'lady_ermine',
+    imagePath: require('../../assets/images/levels/art/medium/Lady_with_an_Ermine.jpg'),
+    isCompleted: false,
+    hint: ""
+  },
+  { answer: 'meninas',
+    imagePath: require('../../assets/images/levels/art/medium/Las_Meninas.jpg'),
+    isCompleted: false,
+    hint: ""
+  },
+  { answer: '_____',
+    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['', '']],
+    hint: ""
+  },
+  { answer: '_____',
+    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['', '']],
+    hint: ""
+  },
+  { answer: '_____',
+    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['', '']],
+    hint: ""
+  },
+  { answer: '_____',
+    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['', '']],
+    hint: ""
+  },
+  { answer: '_____',
+    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['', '']],
+    hint: ""
+  },
+  { answer: '_____',
+    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['', '']],
+    hint: ""
+  },
+  { answer: '_____',
+    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['', '']],
+    hint: ""
+  },
+  { answer: '_____',
+    imagePath: require('../../assets/images/levels/art/medium/_____.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['', '']],
+    hint: ""
+  },
+]
