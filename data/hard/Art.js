630a717d9129dc8722e3937de06aaa972bed64e5
Konstantin Savransky <ksavransky@gmail.com>
2018-10-03 21:55:18 -0700

    done with hardart

--- data/hard/Art.js
+++ data/hard/Art.js
@@ -34,68 +34,69 @@ export const HardArt = [
     imagePath: require('../../assets/images/levels/art/hard/three_graces.jpg'),
     isCompleted: false,
     optionalAnswer: [['trois', 'graces'], ['three', 'graces']],
-    hint: ""
+    hint: "An oil painting by Raphael, depiciting three daughters of Zeus."
   },
   { answer: 'maman',
     imagePath: require('../../assets/images/levels/art/hard/Maman.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "A bronze, stainless steel, and marble sculpture in Ottawa by the artist Louise Bourgeois. The sculpture, which depicts a spider, is among the world's largest."
   },
   { answer: 'manneken_pis',
     imagePath: require('../../assets/images/levels/art/hard/manneken-pis.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "A landmark small bronze sculpture in Brussels, depicting a naked little boy urinating into a fountain's basin."
   },
-  { answer: 'storm_lake_galilee',
+  { answer: 'storm_galilee',
     imagePath: require('../../assets/images/levels/art/hard/Storm_on_the_Lake_of_Galilee.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "A painting from 1633 by Rembrandt. The painting depicts the miracle of Jesus calming a ___ on the lake of ______."
   },
   { answer: 'arnolfini_portrait',
     imagePath: require('../../assets/images/levels/art/hard/The_Arnolfini_Portrait.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "This portrait is a 1434 oil painting by Jan van Eyck. It depicts an Italian merchant and his wife."
   },
   { answer: 'artists_garden_giverny',
     imagePath: require('../../assets/images/levels/art/hard/artists_garden_giverny.jpg'),
     isCompleted: false,
-    optionalAnswer: [['artists', 'garden', 'giverny']],
-    hint: ""
+    optionalAnswer: [['artists', 'garden', 'giverny'], ['lartiste', 'jardin', 'giverny']],
+    hint: "An oil on canvas painting by Monet done in 1900. It is one of many works by the artist of his garden at _____ over the last thirty years of his life."
   },
   { answer: 'school_athens',
     imagePath: require('../../assets/images/levels/art/hard/The_School_of_Athens.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "A fresco by the Italian Renaissance artist Raphael. The figures exemplify Philosophy, Poetry, Theology, and Law in ancient Greece."
   },
   { answer: 'garden_earthly_delights',
     imagePath: require('../../assets/images/levels/art/hard/the-garden-of-earthly-delights.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "A triptych oil painting on oak panel painted by the Early Netherlandish master Hieronymus Bosch."
   },
   { answer: 'rape_proserpina',
     imagePath: require('../../assets/images/levels/art/hard/The-Rape-of-Proserpina-bernini.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "A large Baroque marble sculptural group by Bernini. It depicts the abduction of a goddess, where she is taken to the underworld by Pluto."
   },
   { answer: 'transfiguration',
     imagePath: require('../../assets/images/levels/art/hard/Transfiguration.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "The last painting by Raphael. It depicts the event when Jesus becomes radiant in glory upon a mountain."
   },
   { answer: 'unique_forms_continuity_space',
     imagePath: require('../../assets/images/levels/art/hard/Unique_Forms_of_Continuity_in_Space.jpg'),
     isCompleted: false,
-    hint: ""
+    hint: "A bronze Futurist sculpture by Umberto Boccioni. It is seen as an expression of movement and fluidity. "
   },
   { answer: 'wanderer_above_the_sea_of_fog',
     imagePath: require('../../assets/images/levels/art/hard/Wanderer_above_the_sea_of_fog.jpg'),
+    optionalAnswer: [['wanderer', 'above', 'sea', 'fog'], ['wanderer', 'above', 'mist'], ['mountaineer', 'misty', 'landscape']],
     isCompleted: false,
-    hint: ""
+    hint: "An oil painting by the German Romantic artist Caspar David Friedrich. It has been considered one of the masterpieces of Romanticism."
   },
   { answer: 'winged_victory',
     imagePath: require('../../assets/images/levels/art/hard/Winged_Victory_of_Samothrace_or_Nike.jpg'),
     isCompleted: false,
     optionalAnswer: [['winged', 'victory', 'samothrace'], ['nike', 'samothrace']],
-    hint: ""
+    hint: "A marble Hellenistic sculpture of Nike, that was created about the 2nd century BC."
   }
 ]
