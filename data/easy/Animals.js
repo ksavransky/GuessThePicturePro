fc34bd16abb847c8a2463be050e5e9e381d0830b
Konstantin Savransky <ksavransky@gmail.com>
2018-10-05 09:21:56 -0700

    easy animals

---
+++ data/easy/Animals.js
@@ -0,0 +1,171 @@
+export const EasyAnimals = [
+  { answer: 'bee',
+    imagePath: require('../../assets/images/levels/animals/easy/bee.jpg'),
+    isCompleted: false,
+    hint: "Don't sting me! Go polinate flowers or make honey!"
+  },
+  { answer: 'cat',
+    imagePath: require('../../assets/images/levels/animals/easy/cat.jpg'),
+    isCompleted: false,
+    hint: "Meow. Do you have any mice for me to chase?"
+  },
+  { answer: 'chimpanzee',
+    imagePath: require('../../assets/images/levels/animals/easy/chimpanzee-monkey.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['chimpanzee'], ['monkey']],
+    hint: "Our closest relative. Kind of looks like my uncle."
+  },
+  { answer: 'dog',
+    imagePath: require('../../assets/images/levels/animals/easy/dog.jpg'),
+    isCompleted: false,
+    hint: "Woof! Man's best friend."
+  },
+  { answer: 'donkey',
+    imagePath: require('../../assets/images/levels/animals/easy/donkey.jpg'),
+    isCompleted: false,
+    hint: "Hee-haw! Eeyore in Winnie the Pooh."
+  },
+  { answer: 'elephant',
+    imagePath: require('../../assets/images/levels/animals/easy/elephant.jpg'),
+    isCompleted: false,
+    hint: "Consider to be wise."
+  },
+  { answer: 'fly',
+    imagePath: require('../../assets/images/levels/animals/easy/fly.jpg'),
+    isCompleted: false,
+    hint: "Shoo ___, don't bother me!"
+  },
+  { answer: 'frog',
+    imagePath: require('../../assets/images/levels/animals/easy/frog.jpg'),
+    isCompleted: false,
+    hint: "Ribet! Kermit on the muppets."
+  },
+  { answer: 'giraffe',
+    imagePath: require('../../assets/images/levels/animals/easy/giraffe.jpg'),
+    isCompleted: false,
+    hint: "Long neck for eating leaves from tall trees."
+  },
+  { answer: 'goose',
+    imagePath: require('../../assets/images/levels/animals/easy/goose.jpg'),
+    isCompleted: false,
+    hint: "Always pooping at a park near you."
+  },
+  { answer: 'gorilla',
+    imagePath: require('../../assets/images/levels/animals/easy/gorilla.jpg'),
+    isCompleted: false,
+    hint: "A big ape!"
+  },
+  { answer: 'horse',
+    imagePath: require('../../assets/images/levels/animals/easy/horse.jpg'),
+    isCompleted: false,
+    hint: "Fetch my steed! Replaced by a car."
+  },
+  { answer: 'hummingbird',
+    imagePath: require('../../assets/images/levels/animals/easy/hummingbird.jpg'),
+    isCompleted: false,
+    hint: "Have a very fast heartbeat. Can barely see its wings move. Can hover."
+  },
+  { answer: 'ladybug',
+    imagePath: require('../../assets/images/levels/animals/easy/ladybug.jpg'),
+    isCompleted: false,
+    hint: "The cutest insect?"
+  },
+  { answer: 'leopard',
+    imagePath: require('../../assets/images/levels/animals/easy/leopard.jpg'),
+    isCompleted: false,
+    hint: "Please don't make a _______-print out of this big cat."
+  },
+  { answer: 'lion',
+    imagePath: require('../../assets/images/levels/animals/easy/lion.jpg'),
+    isCompleted: false,
+    hint: "King of the jungle."
+  },
+  { answer: 'moose',
+    imagePath: require('../../assets/images/levels/animals/easy/moose.jpg'),
+    isCompleted: false,
+    hint: "Can find this guy all over Canada, eh?"
+  },
+  { answer: 'mouse',
+    imagePath: require('../../assets/images/levels/animals/easy/mouse.jpg'),
+    isCompleted: false,
+    hint: "Some people keep these little guys as pets. They like cheese."
+  },
+  { answer: 'owl',
+    imagePath: require('../../assets/images/levels/animals/easy/owl.jpg'),
+    isCompleted: false,
+    hint: "Might keep you up at night."
+  },
+  { answer: 'panda',
+    imagePath: require('../../assets/images/levels/animals/easy/panda.jpg'),
+    isCompleted: false,
+    hint: "A type of bear from Asia. Maybe the cutest bear."
+  },
+  { answer: 'panther',
+    imagePath: require('../../assets/images/levels/animals/easy/panther.jpg'),
+    isCompleted: false,
+    hint: "This big black cat is fierce!"
+  },
+  { answer: 'peacock',
+    imagePath: require('../../assets/images/levels/animals/easy/peacock.jpg'),
+    isCompleted: false,
+    hint: "Check out my feathers ladies!"
+  },
+  { answer: 'rabbit',
+    imagePath: require('../../assets/images/levels/animals/easy/rabbit.jpg'),
+    isCompleted: false,
+    hint: "Loves carrots. Very cuddley."
+  },
+  { answer: 'rattlesnake',
+    imagePath: require('../../assets/images/levels/animals/easy/rattlesnake-snake.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['rattlesnake'], ['snake']],
+    hint: ""
+  },
+  { answer: 'rhino',
+    imagePath: require('../../assets/images/levels/animals/easy/rhino.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['rhino'], ['rhinoceros']],
+    hint: "Watch out for its horn!"
+  },
+  { answer: 'rooster',
+    imagePath: require('../../assets/images/levels/animals/easy/rooster-chicken.jpg'),
+    isCompleted: false,
+    optionalAnswer: [['rooster'], ['chicken']],
+    hint: "Might wake you up in the morning if you live on a farm."
+  },
+  { answer: 'shark',
+    imagePath: require('../../assets/images/levels/animals/easy/shark.jpg'),
+    isCompleted: false,
+    hint: "Don't go surfing if this guy is in the water!"
+  },
+  { answer: 'spider',
+    imagePath: require('../../assets/images/levels/animals/easy/spider.jpg'),
+    isCompleted: false,
+    hint: "If it bites you, you may develop super powers and shoot webs from your palms. Or, you might die."
+  },
+  { answer: 'squirrel',
+    imagePath: require('../../assets/images/levels/animals/easy/squirrel.jpg'),
+    isCompleted: false,
+    hint: "Always collecting nuts in parks."
+  },
+  { answer: 'tiger',
+    imagePath: require('../../assets/images/levels/animals/easy/tiger.jpg'),
+    isCompleted: false,
+    hint: "Mike Tyson once had one as a pet!"
+  },
+  { answer: 'turtle',
+    imagePath: require('../../assets/images/levels/animals/easy/turtle.jpg'),
+    isCompleted: false,
+    hint: "Teenage Mutant Ninja ______."
+  },
+  { answer: 'wolf',
+    imagePath: require('../../assets/images/levels/animals/easy/wolf.jpg'),
+    isCompleted: false,
+    hint: "Jack London wrote a lot about these guys."
+  },
+  { answer: 'zebra',
+    imagePath: require('../../assets/images/levels/animals/easy/zebra.jpg'),
+    isCompleted: false,
+    hint: "Look at those stripes."
+  }
+]
